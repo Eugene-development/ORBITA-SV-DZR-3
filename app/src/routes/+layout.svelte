c2a263d483f8a3ae812f810bc95eda4ba177606d
indexpro24@gmail.com <indexpro24@gmail.com>
2022-11-22 21:56:50 +0300

    c

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
     import "../app.css";
     import Banner from "$lib/components/banner/index.svelte"
+    import Header from "$lib/components/header/index.svelte"
     import Footer from "$lib/components/footer/index.svelte"
 </script>
 
 <Banner/>
+<Header/>
 <slot />
 <Footer/>
---
+++ app/src/routes/test/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    /** @type {import('./$types').PageData} */
+</script>
+
+
+<p>123</p>
