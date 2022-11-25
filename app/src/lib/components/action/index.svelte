1f752d4ef7ca71461be323469ffa4a21ccb2ef78
indexpro24@gmail.com <indexpro24@gmail.com>
2022-11-25 22:33:31 +0300

    с

---
+++ app/src/lib/components/action/index.svelte
@@ -0,0 +1,45 @@
+	<div class="my-12 ml-8 text-center text-base">
+		<div>
+			<h2 class="text-base font-semibold uppercase tracking-wide text-cyan-600">Товар месяца</h2>
+			<h3 class="mt-2 text-4xl font-extrabold leading-8 tracking-tight text-cyan-900 sm:text-5xl">
+				Ноябрь-Декабрь
+			</h3>
+		</div>
+	</div>
+
+<div class="relative overflow-hidden bg-gray-50">
+  <div class="mx-auto max-w-7xl">
+    <div class="relative z-10 bg-gray-50 pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-28 xl:pb-32">
+      <svg class="absolute inset-y-0 right-0 hidden h-full w-48 translate-x-1/2 transform text-gray-50 lg:block" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
+        <polygon points="50,0 100,0 50,100 0,100" />
+      </svg>
+
+      <div>
+        <div class="relative px-4 pt-6 sm:px-6 lg:px-8">
+        </div>
+
+      </div>
+
+      <main class="mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+        <div class="sm:text-center lg:text-left">
+          <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+            <span class="block xl:inline">Штукатурка</span> <br>
+            <span class="block text-cyan-600 xl:inline">KNAUF ROTBAND</span>
+          </h1>
+          <p class="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+          <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+            <div class="rounded-md shadow">
+              <a href="/" class="flex w-full items-center justify-center rounded-md border border-transparent bg-cyan-600 px-8 py-3 text-base font-medium text-white hover:bg-cyan-700 md:py-4 md:px-10 md:text-lg">В корзину</a>
+            </div>
+            <div class="mt-3 sm:mt-0 sm:ml-3">
+              <a href="/" class="flex w-full items-center justify-center rounded-md border border-transparent bg-cyan-100 px-8 py-3 text-base font-medium text-cyan-700 hover:bg-cyan-200 md:py-4 md:px-10 md:text-lg">Подробнее</a>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  </div>
+  <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+    <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:h-full lg:w-full" src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80" alt="">
+  </div>
+</div>
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,13 +1,10 @@
 <script>
+	import Action from "$lib/components/action/index.svelte"
 	// export let data
 </script>
-<span on:keypress={() => console.log('123')} on:click={() => console.log('123')}>12345</span>
-
-<h1>Welcome to SvelteKit</h1>
-<p class="text-red-800">
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
 
 <!-- {#each data.data.test888.rubric as {value}}
     {value}
 {/each} -->
+
+<Action/>
