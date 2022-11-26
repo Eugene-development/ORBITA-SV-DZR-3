e701ba10a0f78ad1279e27b74ea85c33ce7f4f8c
indexpro24@gmail.com <indexpro24@gmail.com>
2022-11-26 20:13:04 +0300

    c

--- app/src/routes/blog/post/1/+page.svelte
+++ app/src/routes/blog/post/1/+page.svelte
@@ -1,4 +1,7 @@
 <script>
     /** @type {import('./$types').PageData} */
+    import Post from '$lib/components/post/index.svelte'
     export let data;
 </script>
+
+<Post {...data.contentPost.post}/>
--- app/src/routes/blog/post/2/+page.js
+++ app/src/routes/blog/post/2/+page.js
@@ -1,4 +1,5 @@
 /** @type {import('./$types').PageLoad} */
+import { contentPost } from '$lib/data/blog/post/2';
 export async function load() {
-	return {};
+	return { contentPost };
 }
