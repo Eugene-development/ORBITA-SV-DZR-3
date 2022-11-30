395b2aa963540589311564679dd95234af0af759
indexpro24@gmail.com <indexpro24@gmail.com>
2022-11-30 14:48:52 +0300

    с

--- app/src/lib/components/monthProduct/index.svelte
+++ app/src/lib/components/monthProduct/index.svelte
@@ -10,7 +10,7 @@
 <div class="relative overflow-hidden bg-white">
 	<div class=" mx-auto max-w-full">
 		<div
-			class="pl-20 relative z-10 bg-gray-50 pb-8 sm:pb-12 md:pb-16 lg:w-full lg:max-w-2xl lg:pb-20 xl:pb-24"
+			class="pl-24 relative z-10 bg-gray-50 pb-8 sm:pb-12 md:pb-16 lg:w-full lg:max-w-4xl lg:pb-20 xl:pb-24"
 		>
 			<svg
 				class="absolute inset-y-0 right-0 hidden h-full w-48 translate-x-1/2 transform text-white lg:block"
@@ -69,7 +69,7 @@
 			</main>
 		</div>
 	</div>
-	<div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+	<div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/3">
 		<img
 			class="py-24 h-56 w-full object-cover sm:h-56 md:h-72 lg:h-full lg:w-full hidden md:block"
 			src="https://www.astmnsk.ru/image/cache/data/band2-600x315.jpg"
