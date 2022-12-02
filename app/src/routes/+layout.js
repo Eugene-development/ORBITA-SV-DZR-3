7c1da97717beb1c67c3c408cc6be1f11980ef5c7
indexpro24@gmail.com <indexpro24@gmail.com>
2022-12-02 09:58:57 +0300

    c

--- app/src/routes/+layout.js
+++ app/src/routes/+layout.js
@@ -8,9 +8,11 @@ browser &&
 	localStorage.getItem('session_user') === null &&
 	localStorage.setItem('session_user', crypto.randomUUID());
 browser && sessionUser.update(() => localStorage.getItem('session_user'));
+
 // --- //
 
 // Фиксируем и считаем количество элементов в корзине при перезагрузке страницы и записываем в стор //
+browser && localStorage.getItem('inCart') === null && localStorage.setItem('inCart', []);
 const productsInCart = browser && JSON.parse(localStorage.getItem('inCart'));
 browser && idProductsInCart.update(() => productsInCart);
 browser && lengthCart.update(() => productsInCart?.length);
