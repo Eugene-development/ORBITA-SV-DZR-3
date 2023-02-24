154264cff3fd0ee412022d770a405ab4f418bda4
indexpro24@gmail.com <indexpro24@gmail.com>
2023-02-24 20:18:01 +0300

    с

--- app/src/lib/components/header/nav/information/index.svelte
+++ app/src/lib/components/header/nav/information/index.svelte
@@ -75,18 +75,17 @@
 										class="-m-3 flex items-center rounded-md p-3 text-base font-medium text-slate-900 hover:bg-slate-50"
 									>
 										<svg
-											class="h-6 w-6 flex-shrink-0 text-cyan-700"
 											xmlns="http://www.w3.org/2000/svg"
 											fill="none"
 											viewBox="0 0 24 24"
+											stroke-width="1.5"
 											stroke="currentColor"
-											aria-hidden="true"
+											class="h-5 w-5 flex-shrink-0 text-cyan-900"
 										>
 											<path
 												stroke-linecap="round"
 												stroke-linejoin="round"
-												stroke-width="2"
-												d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+												d="M8.25 4.5l7.5 7.5-7.5 7.5"
 											/>
 										</svg>
 										<span class="ml-4">{displayName}</span>
@@ -106,18 +105,17 @@
 										class="-m-3 flex items-center rounded-md p-3 text-base font-medium text-slate-900 hover:bg-slate-50"
 									>
 										<svg
-											class="h-6 w-6 flex-shrink-0 text-cyan-700"
 											xmlns="http://www.w3.org/2000/svg"
 											fill="none"
 											viewBox="0 0 24 24"
+											stroke-width="1.5"
 											stroke="currentColor"
-											aria-hidden="true"
+											class="h-5 w-5 flex-shrink-0 text-cyan-900"
 										>
 											<path
 												stroke-linecap="round"
 												stroke-linejoin="round"
-												stroke-width="2"
-												d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+												d="M8.25 4.5l7.5 7.5-7.5 7.5"
 											/>
 										</svg>
 										<span class="ml-4">{displayName}</span>
