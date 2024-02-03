f0299dfee657465780483d000a9ac3d73d718adf
Eugene Che <indexpro24@gmail.com>
2024-02-03 23:29:35 +0300

    c

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -30,7 +30,7 @@
 	/>
 </svelte:head>
 
-<section class="py-14">
+<section class="py-16">
 	<div
 		class="grid max-w-full px-2 lg:px-24 py-2 mx-auto lg:gap-8 xl:gap-0 lg:py-2  lg:grid-cols-12"
 	>
@@ -38,19 +38,19 @@
 			<h2
 				class="max-w-7xl mb-4 text-5xl font-bold tracking-tight leading-none md:text-5xl xl:text-8xl dark:text-gray-800"
 			>
-				Весь февраль &#10052;
+				Весь февраль
 			</h2>
 			<p
-				class="max-w-3xl mb-6 font-light text-gray-500 lg:my-8 md:text-lg lg:text-xl dark:text-gray-400"
+				class="max-w-4xl mb-6 font-light text-gray-600 lg:my-8 md:text-lg lg:text-lg dark:text-gray-500"
 			>
 				Отзывчивость к нашим клиентам и стремление быть максимально клиентоориентированными
 				подтолкнули нас к решению продлить акцию. Теперь вы можете воспользоваться <span
-					class="text-red-700">скидкой в 10 процентов</span
+					class="text-red-800">скидкой в 10 процентов</span
 				> не только посетив нашу базу строительных материалов, но и делая заказы через наш интернет-магазин
 				стройматериалов до 29 февраля 2024 года.
 			</p>
 		</div>
-		<div class="hidden px-12 lg:mt-0 lg:col-span-5 lg:flex">
+		<div class="hidden px-16 lg:mt-0 lg:col-span-5 lg:flex">
 			<img
 				src="https://ng-cosmetics.com/upload/iblock/cdf/n2rkpbujez41cs86sa8dtkimg33dfpf8/original_5bf3ee675c35980f8a337b64_5bf3eee504417.jpg"
 				alt="скидка"
