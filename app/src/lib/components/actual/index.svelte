4dfc950c6738238be1ef3b0203c4ee7e8f784d06
Eugene Che <indexpro24@gmail.com>
2024-02-05 08:21:42 +0300

    c

--- app/src/lib/components/actual/index.svelte
+++ app/src/lib/components/actual/index.svelte
@@ -511,7 +511,7 @@
 			Актуальные товары по низким ценам
 		</h2>
 		<p class="py-2 text-xl text-center tracking-tight text-gray-900">
-			Плюс скидка 5% при заказе через интернет магазин стройматериалов.
+			Плюс <span class="text-red-800">скидка 10%</span> при заказе через интернет магазин стройматериалов.
 		</p>
 
 		<div class="mt-8 grid grid-cols-1 gap-y-8 gap-x-6 sm:grid-cols-2 lg:grid-cols-5 xl:gap-x-8">
