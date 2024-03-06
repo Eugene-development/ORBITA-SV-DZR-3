8d2b904e914daa37ba5d1a77b99f7d3fd3be5c2a
Eugene Che <indexpro24@gmail.com>
2024-03-06 15:09:32 +0300

    c

--- app/src/lib/components/actionProduct/index.svelte
+++ app/src/lib/components/actionProduct/index.svelte
@@ -1,3 +1,25 @@
+<script>
+	import { browser } from '$app/environment';
+	import { lengthCart, idProductsInCart } from '$lib/store/stores.js';
+
+	let InCart;
+	idProductsInCart.subscribe((value) => (InCart = value));
+
+	const sendToCart = async (id) => {
+		if (browser && localStorage.getItem('inCart') === null) {
+			browser && localStorage.setItem('inCart', JSON.stringify([id]));
+		} else {
+			const itemsCart = JSON.parse(localStorage.getItem('inCart'));
+			const newItemsCart = [...itemsCart, id];
+			localStorage.setItem('inCart', JSON.stringify(newItemsCart));
+		}
+
+		const productsInCart = JSON.parse(localStorage.getItem('inCart'));
+		lengthCart.update(() => productsInCart.length);
+		idProductsInCart.update(() => productsInCart);
+	};
+</script>
+
 <div class="relative bg-gray-800 my-12 md:my-24">
 	<div
 		class="hidden md:block relative pt-12 h-80 overflow-hidden bg-white md:absolute md:left-0 md:h-full md:w-1/3 lg:w-1/2"
@@ -19,12 +41,31 @@
 				sed. Quam a scelerisque amet ullamcorper eu enim et fermentum, augue. Aliquet amet volutpat
 				quisque ut interdum tincidunt duis.
 			</p>
-			<div class="mt-8">
-				<button
-					class="inline-flex rounded-md bg-white/10 px-8 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-cyan-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-					>В корзину</button
-				>
-			</div>
+			{#if browser && !InCart.some((arrVal) => '431' === arrVal)}
+				<div class="mt-8">
+					<button
+						on:click|preventDefault|once={() => sendToCart('431')}
+						class="inline-flex rounded-md bg-white/10 px-8 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-cyan-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+						>В корзину</button
+					>
+				</div>
+
+				<!-- <div class="rounded-md shadow">
+					<button
+						
+						type="button"
+						class="flex w-full items-center justify-center rounded-md border border-transparent bg-cyan-700 px-8 py-1 text-base font-medium text-white hover:bg-cyan-800  md:px-10 md:text-lg"
+						>В корзину</button
+					>
+				</div> -->
+			{:else}
+				<div class="mt-8">
+					<button
+						class="inline-flex rounded-md px-8 py-2.5 text-sm font-semibold text-white shadow-sm bg-cyan-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+						>В корзине</button
+					>
+				</div>
+			{/if}
 		</div>
 	</div>
 </div>
