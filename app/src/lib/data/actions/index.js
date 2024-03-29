56f397a7b5c692f8e6c0f468f31b423446357230
Eugene Che <indexpro24@gmail.com>
2024-03-29 07:19:12 +0300

    c

--- app/src/lib/data/actions/index.js
+++ app/src/lib/data/actions/index.js
@@ -1,7 +1,7 @@
 const actions = [
 	{
 		id: '2766',
-		action: 'Поликарбонат сотовый прозрачный 4ммc',
+		action: 'Поликарбонат сотовый прозрачный 4мм',
 		price: '6175',
 		description: 'При заказе через сайт дополнительная скидка 5%',
 		link: '/shop/product/polikarbonat-sotovyi-prozracnyi-4mm',
