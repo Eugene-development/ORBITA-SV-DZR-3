8ea49e2f1860c09269de9a3a66b51c94dfe50e01
Eugene Che <indexpro24@gmail.com>
2024-07-01 19:41:04 +0300

    с

--- app/src/lib/components/monthProduct/index.svelte
+++ app/src/lib/components/monthProduct/index.svelte
@@ -59,13 +59,13 @@
 						class="mt-3 text-sm text-gray-700 sm:mx-auto sm:mt-2 sm:max-w-xl sm:text-base md:mt-2 lg:mx-0"
 					>
 						<strong
-							>{dataSlider.price} руб.{#if id === '200'}/м<sup>3</sup>{/if}</strong
+							>Цена - {dataSlider.price} руб.{#if id === '200'}/м<sup>3</sup>{/if}</strong
 						>
-						со скидкой
+						<!-- со скидкой -->
 						<!-- <strong>{Math.round(dataSlider.price)} руб.</strong> со скидкой -->
 						<!-- <strong>{Math.round(dataSlider.price - dataSlider.price * 0.1)} р/шт</strong> со скидкой -->
 						<!-- Цена <strong>{Math.round(dataSlider.price - dataSlider.price * 0.05)} р/шт</strong> со скидкой -->
-						5% через наш строительный интернет магазин
+						<!-- 5% через наш строительный интернет магазин -->
 					</p>
 					<!-- <p
 						class="mt-3 text-base text-gray-700 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0"
