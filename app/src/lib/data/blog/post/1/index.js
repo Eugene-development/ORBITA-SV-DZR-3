5550491bece0d93f3b78575621031c0f55d46d7e
Eugene Che <indexpro24@gmail.com>
2024-07-27 19:14:37 +0300

    с

--- app/src/lib/data/blog/post/1/index.js
+++ app/src/lib/data/blog/post/1/index.js
@@ -3,7 +3,7 @@ const textHTML = `
             <div class="max-w-max lg:max-w-7xl mx-auto">
                 <div class="relative z-10 mb-8 md:mb-2 md:px-6">
                     <div class="text-base max-w-prose lg:max-w-none">
-                        <h2 class="leading-6 text-sky-600 font-semibold tracking-wide uppercase">Пост от 10 мая 2022</h2>
+                        <h2 class="leading-6 text-sky-600 font-semibold tracking-wide uppercase">Пост от 10 мая 2023</h2>
                         <h1 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Покупка стройматериалов в начале сезона</h1>
                     </div>
                 </div>
