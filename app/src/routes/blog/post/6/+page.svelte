a339e7b6443eae52746d93aea4b2b9ccddc74124
Eugene Che <indexpro24@gmail.com>
2024-10-20 18:52:44 +0300

    c

---
+++ app/src/routes/blog/post/6/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	/** @type {import('./$types').PageData} */
+	import Post from '$lib/components/post/index.svelte';
+	export let data;
+</script>
+
+<svelte:head>
+	<title>"Орбита-Строй" | Применение ручного инструмента</title>
+	<meta name="description" content="Информационный пост о применении ручного инструмента" />
+</svelte:head>
+
+<Post {...data.contentPost.post} />
