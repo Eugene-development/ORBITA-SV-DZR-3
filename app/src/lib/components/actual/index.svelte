4f7a7c8353cdc61928528173e652c3d4a24eb665
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-09-11 17:13:29 +0300

    с

--- app/src/lib/components/actual/index.svelte
+++ app/src/lib/components/actual/index.svelte
@@ -55,7 +55,7 @@
 		{
 			id: '228',
 			action: 'Плита ОСБ-3 2500*1250*9мм Kronospan ГОСТ (влагостойкая)',
-			price: '805',
+			price: '775',
 			unit: 'шт',
 			link: '/shop/product/plita-osb-3-250012509mm-Kronospan-gost-vlagostoikaya',
 			img: 'https://lumen-image-bucket.s3.eu-central-1.amazonaws.com/images/ilejI7dCV3zr72h4VudvlNrQLKBgHsrYwKd1HoAL.jpeg'
@@ -90,7 +90,7 @@
 		{
 			id: '2964',
 			action: 'Клей кладочно-монтажный Юнис Униблок 25кг',
-			price: '382',
+			price: '440',
 			unit: 'шт',
 			link: '/shop/product/kliei-kladochno-montazhnyi-iunis-uniblok-25kgh',
 			img: 'https://lumen-image-bucket.s3.eu-central-1.amazonaws.com/images/jf31dasaxfs1y2c2a9u1cm6wmqaja7l5.jpg'
