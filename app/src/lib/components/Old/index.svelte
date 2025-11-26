2f4d8c96e3f2fdbc67bec1d0065f5e71f7bcc655
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-11-26 22:18:12 +0300

    с

---
+++ app/src/lib/components/Old/index.svelte
@@ -0,0 +1,108 @@
+<section class="py-4 lg:py-2 -mt-2">
+	<div class="grid max-w-full px-4 lg:px-12 xl:px-12 mx-auto lg:gap-8 xl:gap-0 lg:grid-cols-12">
+		<div class="mr-auto place-self-center lg:col-span-2">
+			<!-- <div class="lg:flex">
+				<div class="px-6 py-2 rounded-xl bg-gray-50 mt-2">
+					
+					<p class="pb-3 font-extrabold text-red-600 lg:my-4 text-3xl sm:text-4xl xl:text-4xl">
+						Акция!!!
+					</p>
+					<p class=" pb-3 font-extrabold text-green-700 lg:my-4 text-3xl sm:text-4xl xl:text-4xl">
+						Март!!!
+					</p>
+					<p
+						class="font-extrabold text-cyan-700 lg:my-4 text-3xl sm:text-4xl xl:text-4xl whitespace-nowrap"
+					>
+						Сантехника -15%
+					</p>
+					<p
+						class="max-w-3xl font-light text-gray-600 lg:my-8 text-sm md:text-base xl:text-lg dark:text-gray-500"
+					>
+						Предлагаем приобрести продукцию производителя UNIS на нашей базе стройматериалов по
+						специальной цене. Телефон вашего менеджера <span class="text-green-600 font-bold"
+							>8 (902) 303-36-54</span
+						>
+					</p>
+
+					{#if browser && !InCart.some((arrVal) => '431' === arrVal)}
+						<div class="mt-4 md:mt-0">
+							<button
+								on:click|preventDefault|once={() => sendToCart('431')}
+								class="inline-flex rounded-md bg-gray-600 px-16 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-cyan-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+								>В корзину</button
+							>
+						</div>
+					{:else}
+						<div class="mt-4 md:mt-0">
+							<button
+								class="inline-flex rounded-md px-16 py-2.5 text-sm font-semibold text-white shadow-sm bg-cyan-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+								>В корзине</button
+							>
+						</div>
+					{/if}
+				</div>
+
+			</div> -->
+		</div>
+		<div class="lg:ml-0 lg:col-span-4">
+			<img
+				class="h-48 lg:h-[32rem] object-cover mt-8"
+				src="https://storage.yandexcloud.net/orbita/actions/actionOct.jpg"
+				alt="Стройматериалы по акции в Дзержинске"
+			/>
+			<!-- <img
+				class="h-48 lg:h-[36rem] object-cover"
+				src="https://www.kgsnab.ru/upload/iblock/764/764775626c9f841847648937cf438672.jpg"
+				alt="Стройматериалы по акции"
+			/> -->
+		</div>
+		<!-- <div class="lg:ml-0 lg:col-span-4">
+			<img
+				class="h-48 lg:h-96"
+				src="https://www.knauf.ru/upload/iblock/09c/xixblbnsiva2zlg7yenuc21iido7l53k/Rotband_30_1200x900_1.png"
+				alt="Стройматериалы по акции"
+			/>
+		</div> -->
+		<!-- <div class="lg:col-span-3">
+			<img
+				class="h-48 lg:h-80"
+				src="https://vetonit.com/sites/default/files/styles/product_zoom/public/uploads/pim/images/16428/66ef7a12-6517-11ef-a518-e659bb18163c.png?itok=8W_IIcgD"
+				alt="Стройматериалы по акции"
+			/>
+		</div> -->
+		<div class="pl-24 lg:mt-12 lg:col-span-6">
+			<!-- <img
+				class="h-80"
+				src="https://www.knauf.ru/upload/iblock/09c/xixblbnsiva2zlg7yenuc21iido7l53k/Rotband_30_1200x900_1.png"
+				alt="скидка"
+			/> -->
+			<img
+				class="h-48 lg:h-96"
+				src="https://storage.yandexcloud.net/orbita/actions/action2025-04.jpeg"
+				alt="скидка"
+			/>
+			<!-- <img class="" src="https://utepliteli-77.ru/files/brands/unis-logo.png" alt="скидка" /> -->
+			<!-- <img
+				class=""
+				src="https://storage.yandexcloud.net/orbita/actions/-TG40-458-30-09-03-2024%20(1).png"
+				alt="скидка"
+			/> -->
+			<!-- <img
+				class="h-96"
+				src="https://storage.yandexcloud.net/orbita/actions/37520-1000x1000%20(1).jpg"
+				alt="скидка"
+			/> -->
+			<!-- <img
+				class=""
+				src="https://avatars.mds.yandex.net/get-tycoon/742106/2a0000017cbcc1394a569016ca2347baf6b8/priority-promotion"
+				alt="скидка"
+			/> -->
+		</div>
+		<!-- <div class="hidden px-16 lg:mt-0 lg:col-span-5 lg:flex">
+			<img
+				src="https://ng-cosmetics.com/upload/iblock/cdf/n2rkpbujez41cs86sa8dtkimg33dfpf8/original_5bf3ee675c35980f8a337b64_5bf3eee504417.jpg"
+				alt="скидка"
+			/>
+		</div> -->
+	</div>
+</section>
--- app/src/lib/components/holiday/index.svelte
+++ app/src/lib/components/holiday/index.svelte
@@ -2,8 +2,8 @@
 	<div class="mx-auto max-w-7xl px-6 lg:px-8">
 		<div class="mx-auto max-w-2xl lg:max-w-none">
 			<div class="text-center">
-				<h2 class="text-balance text-4xl font-semibold tracking-tight text-red-800 sm:text-7xl">
-					Суперцены ноября!!!
+				<h2 class="text-balance text-4xl font-semibold tracking-tight text-red-700 sm:text-7xl">
+					Суперцены декабря!!!
 				</h2>
 				<p class="mt-4  mx-auto text-xl text-gray-600 max-w-3xl">
 					Воспользуйтесь нашими спецпредложениями и получите значительную выгоду при покупке
@@ -17,6 +17,31 @@
 					Доставка в сезон осуществляется день в день, либо на следующий день от заявки.
 				</p> -->
 			</div>
+			<div class="overflow-hidden bg-white py-4 sm:py-12">
+				<div class="relative isolate">
+					<div class="mx-auto max-w-full ">
+						<div
+							class="mx-auto flex max-w-2xl flex-col gap-16 bg-white/75  shadow-lg ring-1 ring-gray-900/5 sm:rounded-3xl  lg:mx-0 lg:max-w-none lg:flex-row lg:items-center xl:gap-x-20 "
+						>
+							<img
+								src="https://storage.yandexcloud.net/orbita/actions/reklama12.jpg"
+								alt="reklama"
+								class="w-full h-full rounded-2xl object-cover"
+							/>
+						</div>
+					</div>
+					<div
+						aria-hidden="true"
+						class="absolute inset-x-0 -top-16 -z-10 flex transform-gpu justify-center overflow-hidden blur-3xl"
+					>
+						<div
+							style="clip-path: polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)"
+							class="aspect-1318/752 w-329.5 flex-none bg-linear-to-r from-[#9fd6fc] to-[#8680fd] opacity-50"
+						/>
+					</div>
+				</div>
+			</div>
+
 			<!-- <dl
 				class="mt-16 grid grid-cols-1 gap-0.5 overflow-hidden rounded-2xl text-center sm:grid-cols-2 lg:grid-cols-4"
 			>
